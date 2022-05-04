0eab37559a5e41981bab5de0994326283a3bad00
feres1994 <feresfatnassi@gmail.com>
2022-05-04 17:27:32 +0100

    add icons

--- senior-test/src/components/stats/StatsContainer.js
+++ senior-test/src/components/stats/StatsContainer.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import './stats.css'
 
 export default function StatsContainer() {
   return (
-    <div className=' vh-100 d-flex justify-content-center align-items-center primary-font text-primary-light'>
+    <div className=' vh-100 d-flex justify-content-center align-items-center primary-font text-primary-light p-base-medium'>
         <div className='stats-container primary-bg-color padding-16 radius-8'>
            <div className='stats-box-header d-flex align-items-center'>
-               <div className='icon-container secondary-bg-color radius-8 d-flex align-items-center justify-content-center box-shadow m-r-8' >icon</div>
+               <div className='icon-container secondary-bg-color radius-8 d-flex align-items-center justify-content-center box-shadow m-r-8' >
+                   <PersonOutlineOutlinedIcon style={{ color: "blue" }}/>
+               </div>
                <div className='big-number-container d-flex flex-column'>
                    <p>Adverse effect & contraindications</p>
                    <div className='big-number-chart  d-flex'>
@@ -26,10 +30,10 @@ export default function StatsContainer() {
                <div>
                    <p>Similarity Measures</p>
                    <p>between molecules</p>
-                   <div className=' d-flex'>
+                   <div className=' d-flex align-items-center'>
                       <p className='heading-3  text-primary-dark m-r-8'>125%</p>
                       <div>
-                          chart
+                          <p className='text-info-color'><ArrowDropUpIcon  style={{ marginBotom: "-7px", marginRight: "-5px" }}/>  10%</p>
                       </div>
                    </div>
                </div>
